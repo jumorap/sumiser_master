8f6fb7802219ecfe2730c1cfd643ce4756495d32
Jumorap <jumorap@unal.edu.co>
2020-07-25 13:19:51 -0500

    Was build the login layout based in SUMISER project 1. And created the folder 'assets', where are saved all images that look in login_page.dart. Additional it, the connection of Firebase with Android was successfully, and now, our users can use it.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _HomePage();
+}
+
+enum FormType {
+  login,
+  register
+}
+
+class _HomePage extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+enum FormType {
+  login,
+  register
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final formKey = new GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+
+  bool validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void validateAndSubmit() async {
+    if (validateAndSave()) {
+      try {
+        if (_formType == FormType.login) {
+          FirebaseUser user = (await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+              email: _email, password: _password)) as FirebaseUser;
+          print('Signed in: ${user.uid}');
+        } else {
+          FirebaseUser user = (await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(
+              email: _email, password: _password)) as FirebaseUser;
+          print('Registered in: ${user.uid}');
+        }
+      } catch (e) {
+        print('Error: $e');
+      }
+    }
+  }
+
+  void moveToRegister() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin() {
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        // With help of 'SingleChildScrollView' we can build in place of 'Column'
+        // or Container a scrollable layout, giving a beautiful aspect without
+        // damage the composition of screen
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(24.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: buildInputs() + buildSubmitButton(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildInputs() {
+    return <Widget>[
+      Text(
+          "¡Hola! Ingresa tus datos y el código en pantalla",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 22.0,
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.w500,
+          ),
+      ),
+      // We build an image into padding because we are building an app that gonna
+      // looks in little and big phones, and with various proofs, was find
+      // that this is the optimal width, because this class of padding permit
+      // build a padding only in horizontal parts
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Image.asset('assets/login_page_image.png'),
+      ),
+
+      Text(
+        "SUMISER SAS",
+        style: Theme.of(context).textTheme.caption,
+        textAlign: TextAlign.center,
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          // From 'TextFormField' or just 'TextField' we can add images in text boxes
+          // giving nice appearance to text fields
+          prefixIcon: Icon(Icons.person),
+          labelText: 'Email',
+        ),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value,
+      ),
+      TextFormField(
+        // From 'TextFormField' or just 'TextField' we can add images in text boxes
+        // giving nice appearance to text fields
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.lock),
+          labelText: 'Password',
+        ),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButton() {
+    if (_formType == FormType.login) {
+      return <Widget>[
+        // 'SizedBox' build a space enter the text fields and the button
+        SizedBox(
+          height: 15.0,
+        ),
+        Material(
+          child: MaterialButton(
+            minWidth: 150.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+              //side: BorderSide(color: Colors.red),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              'Ingresar',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            onPressed: validateAndSave,
+            highlightColor: Colors.blueAccent[100],
+            color: Colors.blueAccent,
+            textColor: Colors.white,
+          ),
+        ),
+        FlatButton(
+          child: Text(
+            'Create an account',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: moveToRegister,
+        )
+      ];
+    } else{
+      return <Widget>[
+        RaisedButton(
+          child: Text(
+            'Create an account',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: validateAndSave,
+        ),
+        FlatButton(
+          child: Text(
+            'Have an account? Login',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          onPressed: moveToLogin,
+        )
+      ];
+    }
+  }
+}
